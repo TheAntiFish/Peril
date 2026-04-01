405fd88891c0c46f95433c323f6b14f6fe423ac3
TheAntiFish <ethancraigcorcoran@gmail.com>
2026-04-01 13:54:30 +1000

    Added automatic queue creating with client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/TheAntiFish/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/TheAntiFish/learn-pub-sub-starter/internal/pubsub"
+	"github.com/TheAntiFish/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril client...")
+	connectionString := "amqp://guest:guest@localhost:5672/"
+
+	conn, err := amqp.Dial(connectionString)
+	if err != nil {
+		fmt.Printf("Failed to connect to RabbitMQ: %s\n", err)
+		return
+	}
+	defer conn.Close()
+
+	userName, err := gamelogic.ClientWelcome()
+	if err != nil {
+		fmt.Printf("Failed to get client welcome: %s\n", err)
+		return
+	}
+
+	pubsub.DeclareAndBind(conn, routing.ExchangePerilDirect, routing.PauseKey + "." + userName, routing.PauseKey, pubsub.Transient)
+
+	// wait for ctrl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Println("Closing Connection")
 }
