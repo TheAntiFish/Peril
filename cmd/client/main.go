dbf3fa04f751622088a98adeae30bf67e50f53b4
TheAntiFish <ethancraigcorcoran@gmail.com>
2026-04-01 14:15:48 +1000

    Added command loop to clients

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"os/signal"
 
 	"github.com/TheAntiFish/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/TheAntiFish/learn-pub-sub-starter/internal/pubsub"
@@ -29,10 +27,45 @@ func main() {
 
 	pubsub.DeclareAndBind(conn, routing.ExchangePerilDirect, routing.PauseKey + "." + userName, routing.PauseKey, pubsub.Transient)
 
-	// wait for ctrl+c
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan
+	gameState := gamelogic.NewGameState(userName)
 
-	fmt.Println("Closing Connection")
+	clientLoop:
+	for {
+		input := gamelogic.GetInput()
+
+		if len(input) == 0 {
+			continue
+		}
+
+		switch input[0] {
+		case "spawn":
+			err := gameState.CommandSpawn(input)
+			if err != nil {
+				fmt.Printf("Failed to spawn: %s\n", err)
+			}
+		case "move":
+			_, err := gameState.CommandMove(input)
+			if err != nil {
+				fmt.Printf("Failed to move: %s\n", err)
+			}
+		case "status":
+			gameState.CommandStatus()
+		case "help":
+			gamelogic.PrintClientHelp()
+		case "spam":
+			fmt.Println("Spamming not implemented yet!")
+		case "quit":
+			gamelogic.PrintQuit()
+			break clientLoop
+		default:
+			fmt.Printf("Unknown command: %s\n", input[0])
+		}
+	}
+
+	/*
+		// wait for ctrl+c
+		signalChan := make(chan os.Signal, 1)
+		signal.Notify(signalChan, os.Interrupt)
+		<-signalChan
+	*/
 }
